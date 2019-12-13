cb0758c10befaf57b13b4bac12c8b08bc5205468
csmel <melsakas@gmail.com>
2019-12-13 06:54:17 -0500

    I allowed the mapQuest script to select the longitude and longitude variables and they are display on the screen

--- src/App.vue
+++ src/App.vue
@@ -2,104 +2,107 @@
   <div id="app">
     <div class="grid-container fluid">
       <div class="grid-x grid-margin-x">
-        <div class="cell shrink">cell
-          <input ref="autocomplete"
-                 placeholder="Search"
-                 class="search-location"
-                 onfocus="value = ''"
-                 type="text" />
+        <div class="cell small-3">
+          <h5>Search For A Location</h5>
+          <input type="search" id="search-input" placeholder="Start Searching..." />
+<div id="form-longtitude">{{ latitudeDis }}</div>
+
+          <div id="form-latitude">{{ longitudeDis }}</div>
         </div>
-        </div></div>
         <div class="cell auto">
           <div class="grid-y grid-margin-y">
-            <div class="cell main">
-              <div class="grid-x align-spaced grid-margin-x">
+            <div class="cell main-image-parent">
+              <div class="main"></div>
+              <div class="grid-x align-spaced grid-margin-x ">
                 <div class="current-temp cell"><h1>{{ currently.temperature}}&#176;</h1>
-                  <p> {{ currently.summary }}</p>
+                  <p> {{ currently.time }}</p>
+                  <p> {{ currently.timezone }}</p>
+                  {{currently.longitude}}
                   <skycon v-bind:condition="currently.icon"/>
                 </div>
               </div>
-             Daily Forecast
-              <div class=" foo grid-x align-spaced ">
+              Daily Forecast
+              <div class="grid-x align-spaced small-up-2 medium-up-4 large-up-8">
                 <div class="cell small-1" v-for="hour in hourly.data.slice(0, 5)">
-                  <p>{{ hour.time | moment("h a") }}</p>
+                  <h3>{{ hour.time | moment("h a") }}</h3>
                   <skycon v-bind:condition="hour.icon"/>
                   <p>{{ hour.temperature}}&#176;</p>
                 </div>
               </div>
             </div>
-            <div class="cell">
-              <div class=" foo grid-x align-spaced">
-                <div class=" foo grid-x align-spaced">
-                  <div class="cell small-1 alternate-color" v-for="day in daily.data">
-                    <p>{{ day.time | moment("ddd ha") }}</p>
-                    <skycon v-bind:condition="day.icon"/>
-                    <p>{{ day.temperatureHigh}}&#176;</p>
-                    <p>{{ day.temperatureLow}}&#176;</p>
-                  </div>
-                </div>
+            <div class="grid-x grid-padding-x small-up-2 medium-up-4 large-up-8">
+              <div class="cell alternate-color" v-for="day in daily.data">
+                <h4>{{ day.time | moment("ddd") }}</h4>
+                <h5>{{ day.time | moment("Do") }}</h5>
+                <skycon v-bind:condition="day.icon"/>
+                <p>{{ day.temperatureHigh}}&#176;</p>
+                <p>{{ day.temperatureLow}}&#176;</p>
               </div>
             </div>
-            <div class="cell">
-              <div class=" foo grid-x ">
-                <div class=" foo grid-x align-spaced">
-                  <div class="cell small-1">
-                    <h4>Percipitation</h4>
-                    <progress-bar
-                        :options="options"
-                        v-bind:value="currently.nearestStormBearing"
-                    />
-                  </div>
-                  <div class="cell small-1">
-                    <h4>Humidity</h4>
-                    <progress-bar
-                        :options="options"
-                        v-bind:value="currently.humidity"
-                    />
-                  </div>
-                  <div class="cell small-1">
-                    <h4>UV Index</h4>
-                    <progress-bar
-                        :options="options"
-                        v-bind:value="currently.uvIndex"
-                    />
-                  </div>
-                  <div class="cell small-1">
-                   <h4>Max Wind</h4>
-                    <progress-bar
-                        :options="options"
-                        :value="80"
-                    />
-                  </div>
 
+
+            <div class=" grid-x align-spaced small-up-2 medium-up-2 large-up-4">
+              <div class="cell">
+                <h4>Percipitation</h4>
+                <progress-bar
+                    :options="options"
+                    v-bind:value="currently.nearestStormBearing"
+                />
               </div>
-                <div class="cell callout alert auto" v-if="!alerts || !alerts.length">
-                  <h4>Alerts</h4>
-                  <p>No alerts, yet.</p>
-                </div>
-                <div class="cell callout alert auto" v-for="alert in alerts.data" v-else>
-                  <h4>Alerts</h4>
-                  <p>{{ alert.title }}</p>
-                </div>
+              <div class="cell">
+                <h4>Humidity</h4>
+                <progress-bar
+                    :options="options"
+                    v-bind:value="currently.humidity"
+                />
+              </div>
+              <div class="cell">
+                <h4>UV Index</h4>
+                <progress-bar
+                    :options="options"
+                    v-bind:value="currently.uvIndex"
+                />
+              </div>
+              <div class="cell">
+                <h4>Max Wind</h4>
+                <progress-bar
+                    :options="options"
+                    :value="80"
+                />
+              </div>
+              <div class="cell callout alert auto" v-if="!alerts || !alerts.length">
+                <h4>Alerts</h4>
+                <p>No alerts, yet.</p>
+              </div>
+              <div class="cell callout alert auto" v-for="alert in alerts.data" v-else>
+                <h4>Alerts</h4>
+                <p>{{ alert.title }}</p>
+              </div>
             </div>
 
           </div>
+
+
         </div>
       </div>
+    </div>
   </div>
-
-
 </template>
 <script>
   import axios from 'axios';
+
   export default {
     name: 'app',
     data() {
       return {
+        longitudeDis: '',
+        latitudeDis: '',
         address: '',
         day: [],
         hour: [],
-        alert: [],
+        alerts:'',
+currently: '',
+        hourly: '',
         options: {
           text: {
             color: '#fff',
@@ -140,19 +143,7 @@
 
     },
     mounted() {
-      this.autocomplete = new google.maps.places.Autocomplete(
-        (this.$refs.autocomplete),
-        {types: ['geocode']}
-      );
-      this.autocomplete.addListener('place_changed', () => {
-        let place = this.autocomplete.getPlace();
-        let ac = place.address_components;
-        let lat = place.geometry.location.lat();
-        let lon = place.geometry.location.lng();
-        let city = ac[0]["short_name"];
-
-        console.log(`The user picked ${city} with the coordinates ${lat}, ${lon}`);
-      });
+this.waitAminute();
 
       axios.get("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/051b6680f036e325eecb49001964cdae/27.496,-82.594")
         .then(response => {
@@ -172,10 +163,36 @@
         });
 
 
-
     },
     computed: {},
+    methods: {
+      waitAminute: function () {
+        setTimeout(function () {
 
+
+          let ps =  placeSearch({
+            key: 'dlWrWcvQDTvdOpJqrIkkepoKexYGixQa',
+            container: document.querySelector('#search-input'),
+            useDeviceLocation: true,
+            collection: [
+              'poi',
+
+
+            ]
+          });
+
+          ps.on('change', (e) => {
+
+            let longitude = e.result.latlng.lng;
+            let latitude = e.result.latlng.lat;
+            document.querySelector('#form-longtitude').innerHTML =  e.result.latlng.lng ;
+alert( longitude);
+alert(latitude)
+          });
+
+        }, 1000)
+      }
+    }
   }
 </script>
 <style>
@@ -187,32 +204,40 @@
   .current-temp {
     min-height: 10vh;
     max-width: 40vw;
+    background-color: transparent !important;
   }
-  .foo {
-    border:  2px solid red;
-  }
+
 
   .cell {
-border-radius: 5px;
+    border-radius: 5px;
     padding: 10px;
-    min-width: 10vw;
     background-color: #545A5F;
-text-align: center  }
-  .alternate-color:nth-child(odd){
-    background-color: darkcyan;
+    text-align: center;
+
   }
 
+  .alternate-color:nth-child(odd) {
+    background-color: #5C5C5C;
+  }
+.main-image-parent{
+  position: relative;
+  z-index: 1;
+}
   .main {
-    min-height: 50vh;
+    min-height: 100%;
     background-image: url("https://www.sarasotapost.com/images/Bradenton3.jpg");
     background-repeat: no-repeat;
     background-size: cover;
+    opacity: 0.3;
+    position: absolute;
+    width: 100%;
+    z-index: -1;
 
   }
-  .search-location {
-    width: 30vw;
 
-    font-size: 20px;
+  .search-location {
+    width: 15vw;
+
     font-weight: 400;
     outline: none;
     height: 30px;
